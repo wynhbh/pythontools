b8ba2137df4600e40e20970e0fa2e985fc75aebf
wynhbh <wynhbh@gmail.com>
2016-03-10 18:31:18 +0800

    add
    log procedure

---
+++ log/log.py
@@ -0,0 +1,13 @@
+import logging
+import logging.config
+
+logging.config.fileConfig("logging.conf")
+
+logger = logging.getLogger("simpleExample")
+
+logger.debug("debug message")
+logger.info("info message")
+logger.warn("warn message")
+logger.error("error message")
+logger.critical("ciritical message")
+
