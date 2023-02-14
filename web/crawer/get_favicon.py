f1cfabea0332179fe9d4823640d37bb6b6b97662
wynhbh <wynhbh@gmail.com>
2023-02-14 15:44:34 +0800

    update method

--- web/crawer/get_favicon.py
+++ web/crawer/get_favicon.py
@@ -1,9 +1,10 @@
 import mmh3
+import base64
 import requests
 
-favicon_file = 'https://www.google.com/favicon.ico'   #
+favicon_file = 'https://www.baidu.com/favicon.ico'   #
 response = requests.get(favicon_file)
-favicon = response.content.encode('base64')
+favicon = base64.b64encode(response.content)
 hash_result = mmh3.hash(favicon)
 
 print(hash_result)
